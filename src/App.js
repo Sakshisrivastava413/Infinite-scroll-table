ed7868184030523b4da83484b9ac22156f9c7e98
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-08-10 18:05:24 +0530

    added table info in the view fetching it on scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import { InfinityTable as Table } from 'antd-table-infinity';
-import 'antd-table-infinity/index.css'
+import { columns, fetchData } from './mockData';
 
 class App extends Component {
   state = {
     data: [],
     loading: false,
   };
-
- 
+  handleFetch = () => {
+    console.log('loading');
+    this.setState({ loading: true });
+    fetchData(this.state.data.length).then(newData =>
+      this.setState(({ data }) => ({
+        loading: false,
+        data: data.concat(newData),
+      })),
+    );
+  };
 
   render() {
     return (
       <Table
         key="key"
         loading={this.state.loading}
+        onFetch={this.handleFetch}
         pageSize={100}
+        columns={columns}
         scroll={{ y: 450 }}
         dataSource={this.state.data}
         bordered
