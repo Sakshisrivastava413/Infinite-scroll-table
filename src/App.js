971075609cb1f4aaa1926e7574e5b637e888801e
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-08-10 22:19:41 +0530

    app and code design modifications

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react';
 import { Spin } from 'antd';
-import { InfinityTable as Table } from 'antd-table-infinity';
 import { columns, fetchData } from './mockData';
+import { InfinityTable as Table } from 'antd-table-infinity';
+import './App.css';
+import 'antd-table-infinity/dist/index.css';
+
+const LoadingIndicator = (
+	<div className="loading-indicator-container">
+		<Spin tip="Loading..." />
+	</div>
+);
 
 class App extends Component {
-  state = {
-    data: [],
-    loading: false,
-  };
-  handleFetch = () => {
-    console.log('loading');
-    this.setState({ loading: true });
-    fetchData(this.state.data.length).then(newData =>
-      this.setState(({ data }) => ({
-        loading: false,
-        data: data.concat(newData),
-      })),
-    );
-  };
-  loadMoreContent = () => (
-    <div
-      style={{
-        textAlign: 'center',
-        paddingTop: 40,
-        paddingBottom: 40,
-        border: '1px solid #e8e8e8',
-      }}
-    >
-      <Spin tip="Loading..." />
-    </div>
-  );
 
-  render() {
-    return (
-      <Table
-        key="key"
-        loading={this.state.loading}
-        onFetch={this.handleFetch}
-        pageSize={100}
-        columns={columns}
-        loadingIndicator={this.loadMoreContent()}
-        scroll={{ y: 450 }}
-        dataSource={this.state.data}
-        bordered
-        debug
-      />
-    );
-  }
+	state = {
+		data: [],
+		loading: false,
+	};
+
+	handleFetch = () => {
+		console.log('loading');
+		this.setState({ loading: true });
+		fetchData(this.state.data.length).then(newData =>
+			this.setState(({ data }) => ({
+				loading: false,
+				data: data.concat(newData),
+			})),
+		);
+	};
+
+	render() {
+		return (
+			<div>
+				<h3 className="title">Welcome.</h3>
+				<Table
+					key="key"
+					loading={this.state.loading}
+					onFetch={this.handleFetch}
+					pageSize={100}
+					columns={columns}
+					loadingIndicator={LoadingIndicator}
+					scroll={{ y: 450 }}
+					dataSource={this.state.data}
+					bordered
+					debug
+				/>
+			</div>
+		);
+	}
 }
 
 
--- src/mockData.js
+++ src/mockData.js
@@ -2,47 +2,47 @@
 import { random } from 'lodash';
 
 const fetchData = (startIndex = 0) =>
-  new Promise(resolve => {
-    setTimeout(() => {
-      resolve(
-        startIndex >= 1000
-          ? []
-          : Array.from({ length: 50 }).map((_, i) => {
-              const index = startIndex + i;
-              return {
-                key: i,
-                index: `${index}`,
-                name: 'John Brown',
-                age: 32,
-                address: 'New York No. 1 Lake Park',
-              };
-            }),
-      );
-    }, random(0, 1.0) * 1000);
-  });
+	new Promise(resolve => {
+		setTimeout(() => {
+			resolve(
+				startIndex >= 1000
+					? []
+					: Array.from({ length: 50 }).map((_, i) => {
+						const index = startIndex + i;
+						return {
+							key: index,
+							index: `${index}`,
+							name: 'John Brown',
+							age: 32,
+							address: 'New York No. 1 Lake Park',
+						};
+					}),
+			);
+		}, random(0, 1.0) * 1000);
+	});
 
 const columns = [
-  {
-    title: 'index',
-    dataIndex: 'index',
-    render: text => text,
-    width: 50,
-  },
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    width: 100,
-  },
-  {
-    title: 'Age',
-    width: 50,
-    dataIndex: 'age',
-  },
-  {
-    title: 'Address',
-    width: 200,
-    dataIndex: 'address',
-  },
+	{
+		title: 'Id',
+		dataIndex: 'index',
+		render: text => text,
+		width: 50,
+	},
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		width: 100,
+	},
+	{
+		title: 'Age',
+		width: 50,
+		dataIndex: 'age',
+	},
+	{
+		title: 'Address',
+		width: 200,
+		dataIndex: 'address',
+	},
 ];
 
 export { columns, fetchData };
